8faa355569250456663a193df64f640b9cc2d9dd
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2023-12-23 12:55:38 -0300

    Adds audio conversion to plugin

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import requests
 import logging
+import requests
+import hashlib
 
 from typing import Any
 from llm import get_llm_class
-from fastapi import APIRouter, Body, HTTPException
+from fastapi import APIRouter, Body, HTTPException, Query, Depends, Request
+from openai import OpenAI
 
 from settings import (
     WHATSAPP_VERIFY_TOKEN,
@@ -14,18 +16,23 @@ from settings import (
 
 from models.helpers import create_session
 from webhooks.responses import whatsapp_get_response
+from uuid import uuid4
+
+
+client = OpenAI()
 
 router = APIRouter()
 
 logger = logging.getLogger(__name__)
 
 @router.get("/whats-audio")
-async def whats_audio_get(request):
+async def whats_audio_get(request: Request):
     content = await whatsapp_get_response(request)
     return content
 
 @router.post("/whats-audio")
-async def whats_audio_post(request, body: Any = Body(None)):
+async def whats_audio_post(body: Any = Body(None)):
+    logger.info("Started")
     value = body["entry"][0]["changes"][0]["value"]
     try:
         message = value["messages"][0]["text"]["body"]
@@ -33,7 +40,7 @@ async def whats_audio_post(request, body: Any = Body(None)):
         raise HTTPException(status_code=200)
 
     from_number = value["messages"][0]["from"]
-    logger.info(value)
+    logger.info(f"Got message from {from_number} - message: {message}")
     headers = {
         "Authorization": f"Bearer {WHATSAPP_API_TOKEN}",
         "Content-Type": "application/json",
@@ -48,14 +55,29 @@ async def whats_audio_post(request, body: Any = Body(None)):
     processed_message = processed_message["text"]
     logger.info("Processed message: %s", processed_message)
     # Generate the audio here
-    # data = {
-    #     "messaging_product": "whatsapp",
-    #     "to": from_number,
-    #     "type": "text",
-    #     "text": {"body": processed_message},
-    # }
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code not in [200, 201]:
-        logger.info(f"Failed request: {response.text}")
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=processed_message,
+        response_format="mp3"
+    )
+    # generate hash for file name
+    file_hash = uuid4().hex
+    filename = f"{file_hash}.mp3"
+    response.stream_to_file(f"/app/static/{filename}")
+    # Save response to a memory file
+    logger.info("Got audio back from OpenAI")
 
-    return response.json()
+    data = {
+        "messaging_product": "whatsapp",
+        "to": from_number,
+        "type": "audio",
+        "audio": {"link":  "https://api.talkd.ai/static/" + filename},
+    }
+    response = requests.post(url, json=data, headers=headers)
+
+    # Posting media to WhatsApp API
+    logger.info(f"Posted audio to WhatsApp API - {response.request.url} - {response.status_code} - {response.text}")
+    response.raise_for_status()
+
+    return {}
